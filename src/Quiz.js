048e362f73c78df046e7b8db134e40d8394f438a
senthilrl <73290483+senthilrl@users.noreply.github.com>
2021-03-23 17:35:17 +0530

    Completed Module 8

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,12 +15,16 @@ class Quiz extends Component{
         this.setState({quiz_position:this.state.quiz_position+1});
     }
 
+    handleResetClick(){
+        this.setState({quiz_position:1});
+    }
+
     render(){
         const isQuizEnd=this.state.quiz_position-1==quizData.quiz_questions.length;
         let display;
         if(isQuizEnd)
         {
-            display=<QuizEnd/>;
+            display=<QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>;
         }
         else
         {
